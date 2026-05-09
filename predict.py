a901a946938d6e57f7d9cd61d45268615c1b3750
IsuruMadhushan0430 <155760402+IsuruMadhushan0430@users.noreply.github.com>
2026-05-09 15:07:03 +0530

    Implement prediction script for dog breed classification

---
+++ predict.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.preprocessing import image
+
+model = tf.keras.models.load_model("models/dog_breed_model.h5")
+
+labels = pd.read_csv("dataset/labels.csv")
+encoder = LabelEncoder()
+encoder.fit(labels['breed'])
+
+img_path = "dataset/test/0a8d8dda0e354c0571c8d47600ab39a3.jpg"
+
+img = image.load_img(img_path, target_size=(224,224))
+img_array = image.img_to_array(img)
+img_aaray = img_array / 255.0
+
+img_array = np.expand_dims(img_array, axis=0)
+prediction = model.predict(img_array)
+
+predicted_index = np.argmax(prediction)
+predicted_breed = encoder.inverse_transform([predicted_index])
+print("Predicted breed:", predicted_breed[0])
